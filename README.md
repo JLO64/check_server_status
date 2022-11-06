541e7960d61f8704271484b2946c793284f73c3d
“JLO64” <64julianlopez@gmail.com>
2022-11-06 09:38:41 -0800

    Additional launch options, Docker error message

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 A script made to check the status of a Linux server that is meant to be run locally on it. I use it with Shortcuts on my iPhone over SSH to check the status of my home server with Siri.
 
+I mainly wrote this script to try out GitHub Copilot and try and make a script that's not 100% jank with has some documentation.
+
 Suggestions and bug reports are appreciated!
 
 ## Prerequisites
@@ -15,6 +17,10 @@ Add this line to your sudo crontab file (be sure to change the user in the path)
     */1 * * * * docker ps -a --format "{{.Names}}" --filter status=running > /home/user/.docker_running.txt
 
 You can change the path to wherever you want, but make sure you change the txt file path in the script (variable *docker_active_txt*) to match the path used in the crontab file.
+
+If you have improperly set up your crontab command, the following message will be shown.
+	
+	Unable to locate docker_running.txt file. Please check if your sudo crontab is properly configured."
 
 If you are not running Docker as root, you will need to use the __non-root__ user's crontab file.
 
@@ -31,13 +37,17 @@ Otherwise, you can run it using python:
 
 ## Options
 
-    -a, --all             Check all status
-    -v, --version         Show program's version number and exit
-    -h, --help            Show this help message and exit
+	-a, --all              Check all status
+	-d, --docker           Check docker status
+	-v, --virtual-machines Check virtual machines status
+	-p, --packages         Check number of upgradable packages
+	-u, --uptime           Check server uptime
+	-h, --help             Show this help message and exit
+	--version              Show program's version number and exit
 
 ## Output Examples
 
-### Without any arguments / with "--all" argument
+### Without any arguments / with "-a" / "--all" argument
 Command
 
     $ ./server_status.py
@@ -50,6 +60,45 @@ Output
     There are currently 3 upgradable packages.
     Currently there are 2 running docker containers: (rutorrent jellyfin)
     There are no running virtual machines.
+
+### With "-d" / "--docker" argument
+Command
+
+	$ ./server_status.py -d
+	or
+	$ ./server_status.py --docker
+Output
+
+	Currently there are 2 running docker containers: (rutorrent jellyfin)
+
+### With "-v" / "--virtual-machines" argument
+Command
+
+	$ ./server_status.py -v
+	or
+	$ ./server_status.py --virtual-machines
+Output
+
+	There are no running virtual machines.
+
+### With "-p" / "--packages" argument
+Command
+
+	$ ./server_status.py -p
+	or
+	$ ./server_status.py --packages
+Output
+	
+	There are currently 3 upgradable packages.
+### With "-u" / "--uptime" argument
+Command
+
+	$ ./server_status.py -u
+	or
+	$ ./server_status.py --uptime
+Output
+
+	Your server has been up 6 hours, 47 minutes
 
 
 ## Credits
--- server_status.py
+++ server_status.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 #Written by Julian Lopez and GitHub Copilot
 
-import subprocess
+import subprocess, os
 
 version_number = "1.0.0"
 
@@ -12,9 +12,13 @@ def check_args():
 	if len(sys.argv) == 1: main()
 	elif len(sys.argv) == 2:
 		if ((sys.argv[1] == "-a" or sys.argv[1] == "--all" )): main()
-		elif ((sys.argv[1] == "-v" or sys.argv[1] == "--version" )): show_version()
+		elif ((sys.argv[1] == "--version" )): show_version()
         elif ((sys.argv[1] == "-h" or sys.argv[1] == "--help" )): show_help()
-		else: print ( "Invalid argument" )
+		elif ((sys.argv[1] == "-d" or sys.argv[1] == "--docker" )): print ( get_docker_info() )
+		elif ((sys.argv[1] == "-u" or sys.argv[1] == "--uptime" )): print ( get_server_uptime().replace('\n', ' ') )
+		elif ((sys.argv[1] == "-p" or sys.argv[1] == "--packages" )): print ( get_num_of_upgradable_packages() )
+		elif ((sys.argv[1] == "-v" or sys.argv[1] == "--virtual-machines" )): print ( get_virsh_info() )
+		else: print ( "Invalid argument. Please use -h or --help for help." )
 	else: print ( "Invalid arguments" )
 
 def line_count(filename):
@@ -26,10 +30,12 @@ def read_lines_without_newline(filename):
         return fp.read().replace('\n', ' ')
 
 def get_docker_info():
-	docker_active_lines = line_count(docker_active_txt)
-	if docker_active_lines == 0: return ( "There are no running docker containers.")
-	elif docker_active_lines == 1: return ( "Currently there is one running docker container: " + read_lines_without_newline(docker_active_txt)[:-1] + ")" )
-	else: return ( "Currently there are " + str(docker_active_lines) + " running docker containers: (" + read_lines_without_newline(docker_active_txt)[:-1] + ")" )
+	if os.path.exists(docker_active_txt):
+		docker_active_lines = line_count(docker_active_txt)
+		if docker_active_lines == 0: return ( "There are no running docker containers.")
+		elif docker_active_lines == 1: return ( "Currently there is one running docker container: " + read_lines_without_newline(docker_active_txt)[:-1] + ")" )
+		else: return ( "Currently there are " + str(docker_active_lines) + " running docker containers: (" + read_lines_without_newline(docker_active_txt)[:-1] + ")" )
+	else: return ( "Unable to locate docker_running.txt file. Please check if your sudo crontab is properly configured." )
 
 def get_virsh_info():
 	active_vms = subprocess.run("virsh -c qemu:///system list --name", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -51,9 +57,13 @@ def show_help():
 	print ( "Usage: server_status.py [OPTION]" )
 	print ( "Show the status of your server." )
 	print ( " " )
-	print ( "  -a, --all             Check all status" )
-	print ( "  -v, --version         Show program's version number and exit" )
-	print ( "  -h, --help            Show this help message and exit" )	
+	print ( "  -a, --all              Check all status" )
+	print ( "  -d, --docker           Check docker status" )
+	print ( "  -v, --virtual-machines Check virtual machine status" )
+	print ( "  -p, --packages         Check number of upgradable packages" )
+	print ( "  -u, --uptime           Check server uptime" )
+	print ( "  -h, --help             Show this help message and exit" )
+	print ( "  --version              Show program's version number and exit" )
 	
 def show_version():
 	print ( "server_status.py version " + version_number )
